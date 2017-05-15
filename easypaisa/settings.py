619fb68ed67af07493898d2112718c4126079530
qasimkhokhar <qasim.gulzar52@gmail.com>
2017-05-16 01:06:20 +0500

    [FIX] Django Rest Framework - Missing Static Directory.

--- easypaisa/settings.py
+++ easypaisa/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.abspath('../')
 STATIC_ROOT =  os.path.join(PROJECT_ROOT,'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    STATIC_ROOT,
 )
 
 REST_FRAMEWORK = {
