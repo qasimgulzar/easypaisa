90185378865ca17c3d7d65e59159934c8b9b7134
qasimkhokhar <qasim.gulzar52@gmail.com>
2017-05-16 23:07:23 +0500

    [FIX] add authentication api in payments module.

--- easypaisa_module/urls.py
+++ easypaisa_module/urls.py
@@ -8,6 +8,7 @@ router = routers.DefaultRouter()
 router.register(r'status', PaymentStatusViewSet)
 urlpatterns=[
     url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^postbackhandler/$',csrf_exempt(PostbackHandler.as_view())),
     url(r'^start/$',PaymentView.as_view())
 ]
