2489c0ffcfe953e0b8c07fd9227e094cce44db3d
qasimkhokhar <qasim.gulzar52@gmail.com>
2017-05-16 22:18:38 +0500

    [FIX] import settings from django.conf.

--- main/views.py
+++ main/views.py
@@ -8,8 +8,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.base import View
 from rest_framework import serializers, viewsets
-from easypaisa.settings import STORE_ID, PAYMEN_TEMPLATE, PAY_TEMPLATE, EASYPAISA_POST_BACK_URL, EASYPAISA_SERVER_URL, \
-    PAYMENT_COMPLETION_REDIRECT_TO_URL
+from django.conf import settings
 # Serializers define the API representation.
 from main.models import PaymentStatusModel
 
@@ -25,14 +24,17 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class PaymentStatusSerializer(serializers.ModelSerializer):
     class Meta:
-        model=PaymentStatusModel
-        exclude=()
+        model = PaymentStatusModel
+        exclude = ()
+
 
 class PaymentStatusViewSet(viewsets.ModelViewSet):
-    queryset=PaymentStatusModel.objects.all()
+    queryset = PaymentStatusModel.objects.all()
     serializer_class = PaymentStatusSerializer
+
 
 class PostbackHandler(ListView):
     def get(self, request):
@@ -41,18 +43,19 @@ class PostbackHandler(ListView):
         if (loopcount == 1):
             context = {
                 'auth_token': request.GET.get('auth_token'),
-                'postBackURL': '%s?loopcount=%s' % (EASYPAISA_POST_BACK_URL, loopcount),
-                'EASYPAISA_SERVER_URL': EASYPAISA_SERVER_URL
+                'postBackURL': '%s?loopcount=%s' % (settings.EASYPAISA_POST_BACK_URL, loopcount),
+                'EASYPAISA_SERVER_URL': settings.EASYPAISA_SERVER_URL
             }
         if loopcount == 2:
             paymentStatus = PaymentStatusModel(status=request.GET.get('status', ''), desc=request.GET.get('desc', ''),
                                                orderRefNumber=request.GET.get('orderRefNumber', ''))
             paymentStatus.save()
-            return HttpResponseRedirect(PAYMENT_COMPLETION_REDIRECT_TO_URL)
-        return render(request, PAY_TEMPLATE, context)
+            return HttpResponseRedirect(settings.PAYMENT_COMPLETION_REDIRECT_TO_URL)
+        return render(request, settings.PAY_TEMPLATE, context)
 
 
 class PaymentView(View):
     def get(self, request):
-        context = {'store_id': STORE_ID, 'EASYPAISA_SERVER_URL': EASYPAISA_SERVER_URL,'postBackURL':EASYPAISA_POST_BACK_URL}
-        return render(request, PAYMEN_TEMPLATE, context)
+        context = {'store_id': settings.STORE_ID, 'EASYPAISA_SERVER_URL': settings.EASYPAISA_SERVER_URL,
+                   'postBackURL': settings.EASYPAISA_POST_BACK_URL}
+        return render(request, settings.PAYMEN_TEMPLATE, context)
